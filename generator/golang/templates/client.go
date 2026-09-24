df1b41d3d88948db83bfc048090a04ddedc2df18
TimAndy <xuchonglei@126.com>
2026-09-24 16:12:25 +0800

    [代码生成]-简化生成的代码, 去除入参 context 和返回值 error

--- generator/golang/templates/client.go
+++ generator/golang/templates/client.go
@@ -75,6 +75,7 @@ func (p *{{$ClientName}}) {{- template "FunctionSignature" . -}} {
 	{{if .Streaming.IsStreaming -}}
 	panic("streaming method {{$ServiceName}}.{{.Name}}(mode = {{.Streaming.Mode}}) not available, please use Kitex Thrift Streaming Client.")
 	{{else -}}
+	ctx := context.Background()
 	var _args {{$ArgType.GoName}}
 	{{- range .Arguments}}
 	_args.{{($ArgType.Field .Name).GoName}} = {{.GoName}}
@@ -82,39 +83,37 @@ func (p *{{$ClientName}}) {{- template "FunctionSignature" . -}} {
 
 	{{- if .Void}}
 	{{- if .Oneway}}
-	if err = p.Client_().Call(ctx, "{{.Name}}", &_args, nil); err != nil {
-		return
+	if err := p.Client_().Call(ctx, "{{.Name}}", &_args, nil); err != nil {
+		panic(err)
 	}
 	{{- else}}
 	var _result {{$ResType.GoName}}
-	if err = p.Client_().Call(ctx, "{{.Name}}", &_args, &_result); err != nil {
-		return
+	if err := p.Client_().Call(ctx, "{{.Name}}", &_args, &_result); err != nil {
+		panic(err)
 	}
 	{{- if .Throws}}
-	switch {
 	{{- range .Throws}}
-	case _result.{{($ResType.Field .Name).GoName}} != nil:
-		return _result.{{($ResType.Field .Name).GoName}}
-	{{- end}}
+	if _result.{{($ResType.Field .Name).GoName}} != nil {
+		panic(_result.{{($ResType.Field .Name).GoName}})
 	}
+	{{- end}}
 	{{- end}}
 
 	{{- end}}
-	return nil
+	return
 	{{- else}}{{/* If .Void */}}
 	var _result {{$ResType.GoName}}
-	if err = p.Client_().Call(ctx, "{{.Name}}", &_args, &_result); err != nil {
-		return
+	if err := p.Client_().Call(ctx, "{{.Name}}", &_args, &_result); err != nil {
+		panic(err)
 	}
 	{{- if .Throws}}
-	switch {
 	{{- range .Throws}}
-	case _result.{{($ResType.Field .Name).GoName}} != nil:
-		return r, _result.{{($ResType.Field .Name).GoName}}
-	{{- end}}
+	if _result.{{($ResType.Field .Name).GoName}} != nil {
+		panic(_result.{{($ResType.Field .Name).GoName}})
 	}
 	{{- end}}
-	return _result.GetSuccess(), nil
+	{{- end}}
+	return _result.GetSuccess()
 	{{- end}}{{/* If .Void */}}
 	{{- end}}{{/* If .Streaming.IsStreaming */ -}}
 }
